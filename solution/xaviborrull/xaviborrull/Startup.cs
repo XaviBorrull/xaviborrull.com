1ebda788290738f88e1ce090ce388a7980ccef7f
Xavi Borrull <xaviborrull@gmail.com>
2017-08-30 12:23:50 +0200

    Solution created

---
+++ solution/xaviborrull/xaviborrull/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(xaviborrull.Startup))]
+namespace xaviborrull
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
